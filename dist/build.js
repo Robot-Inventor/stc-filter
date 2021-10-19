ff9cb4902249c2a51c09134f32f4afed3a0fd290
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2021-10-20 01:40:56 +0900

    フィルターの形式のリントの実装を改善 #113

--- dist/build.js
+++ dist/build.js
@@ -2,92 +2,104 @@
 const fs = require("fs");
 const path = require("path");
 const { parse } = require("jsonc-parser");
-function lint_query_object(input) {
-    const is_object = typeof input === "object" && input.length === undefined;
-    if (!is_object)
-        return [false, "Input must be an object."];
-    const has_rule = "rule" in input;
-    if (!has_rule)
-        return [false, "Input object must have rule property."];
-    const is_type_of_rule_right = lint_query_type(input.rule);
-    if (!is_type_of_rule_right[0])
-        return is_type_of_rule_right;
-    return [true];
+const judge_object = (input) => {
+    return typeof input === "object" && input !== null && input.constructor === Object;
+};
+class Lint {
+    run(input) {
+        return this.lint_query_object(input);
+    }
+    lint_query_object(input) {
+        if (!judge_object(input))
+            return [false, "Input must be an object."];
+        const has_rule = "rule" in input;
+        if (!has_rule)
+            return [false, "Input object must have rule property."];
+        const is_type_of_rule_right = this.lint_query_type(input.rule);
+        if (!is_type_of_rule_right[0])
+            return is_type_of_rule_right;
+        return [true];
+    }
+    lint_query_type(input) {
+        if (!Array.isArray(input))
+            return [false, "query_type must be an array."];
+        const is_and_or = ["and", "or"].includes(input[0]);
+        if (!is_and_or)
+            return [false, "The first content of query_type must be \"and\" or \"or\"."];
+        const is_length_2 = input.length === 2;
+        if (!is_length_2)
+            return [false, "Length of query_type must be 2."];
+        const is_second_value_array = Array.isArray(input[1]);
+        if (!is_second_value_array)
+            return [false, "The second content of query_type must be an array."];
+        const is_type_of_second_value_right = (() => {
+            let result = [true];
+            input[1].forEach((e) => {
+                const is_array = Array.isArray(e);
+                const is_object = judge_object(e);
+                if (is_array) {
+                    const lint_result = this.lint_query_type(e);
+                    if (!lint_result[0])
+                        result = lint_result;
+                }
+                else if (is_object) {
+                    const lint_result = this.lint_query_element(e);
+                    if (!lint_result[0])
+                        result = lint_result;
+                }
+                else {
+                    result = [false, "The type of the second content of query_type must be query_type or query_object"];
+                }
+            });
+            return result;
+        })();
+        if (!is_type_of_second_value_right[0])
+            return is_type_of_second_value_right;
+        return [true];
+    }
+    lint_query_element(input) {
+        const has_mode = "mode" in input;
+        if (!has_mode)
+            return [false, "query_element must have mode property"];
+        const is_mode_right = ["include", "exclude"].includes(input.mode);
+        if (!is_mode_right)
+            return [false, "The value of mode property of query_element must be \"include\" or \"exclude\"."];
+        const has_type = "type" in input;
+        if (!has_type)
+            return [false, "query_element must have type property"];
+        const is_type_right = ["text", "hashtag", "name", "id", "link"].includes(input.type);
+        if (!is_type_right)
+            return [false, "The value of type property of query_element must be \"text\", \"hashtag\", \"name\", \"id\" or \"link\"."];
+        const has_string = "string" in input;
+        if (!has_string)
+            return [false, "query_element must have string property."];
+        const is_type_of_string_right = typeof input.string === "string";
+        if (!is_type_of_string_right)
+            return [false, "The type of string property of query_element must be string."];
+        return [true];
+    }
 }
-function lint_query_type(input) {
-    const is_array = typeof input === "object" && input.length !== undefined;
-    if (!is_array)
-        return [false, "query_type must be an array."];
-    const is_and_or = ["and", "or"].includes(input[0]);
-    if (!is_and_or)
-        return [false, "The first content of query_type must be \"and\" or \"or\"."];
-    const is_length_2 = input.length === 2;
-    if (!is_length_2)
-        return [false, "Length of query_type must be 2."];
-    const is_second_value_array = typeof input[1] === "object" && input[1].length !== undefined;
-    if (!is_second_value_array)
-        return [false, "The second content of query_type must be an array."];
-    const is_type_of_second_value_right = (() => {
-        let result = [true];
-        input[1].forEach((e) => {
-            const is_array = typeof e === "object" && e.length !== undefined;
-            const is_object = typeof e === "object" && e.length === undefined;
-            if (is_array) {
-                const lint_result = lint_query_type(e);
-                if (!lint_result[0])
-                    result = lint_result;
-            }
-            else if (is_object) {
-                const lint_result = lint_query_element(e);
-                if (!lint_result[0])
-                    result = lint_result;
-            }
-            else {
-                result = [false, "The type of the second content of query_type must be query_type or query_object"];
-            }
-        });
-        return result;
-    })();
-    if (!is_type_of_second_value_right[0])
-        return is_type_of_second_value_right;
-    return [true];
-}
-function lint_query_element(input) {
-    const has_mode = "mode" in input;
-    if (!has_mode)
-        return [false, "query_element must have mode property"];
-    const is_mode_right = ["include", "exclude"].includes(input.mode);
-    if (!is_mode_right)
-        return [false, "The value of mode property of query_element must be \"include\" or \"exclude\"."];
-    const has_type = "type" in input;
-    if (!has_type)
-        return [false, "query_element must have type property"];
-    const is_type_right = ["text", "hashtag", "name", "id", "link"].includes(input.type);
-    if (!is_type_right)
-        return [false, "The value of type property of query_element must be \"text\", \"hashtag\", \"name\", \"id\" or \"link\"."];
-    const has_string = "string" in input;
-    if (!has_string)
-        return [false, "query_element must have string property."];
-    const is_type_of_string_right = typeof input.string === "string";
-    if (!is_type_of_string_right)
-        return [false, "The type of string property of query_element must be string."];
-    return [true];
-}
-function lint(input) {
-    return lint_query_object(input);
-}
+const generate_cdn_url = (relative_path) => {
+    return `https://cdn.statically.io/gh/Robot-Inventor/stc-filter/main/${relative_path}`;
+};
+const get_filter_list = () => {
+    return parse(fs.readFileSync("./src/filter/filter_list.json").toString());
+};
 try {
-    const file_list = parse(fs.readFileSync("./src/filter/filter_list.json").toString());
+    const filter_list = get_filter_list();
     const advanced_filter = {};
-    file_list.filter.forEach((filter) => {
+    filter_list.filter.forEach((filter) => {
         const json_file_list = fs.readdirSync(path.join("./src/filter", filter.dir));
         let filter_content = [];
+        const lint = new Lint();
         json_file_list.forEach((file_name) => {
+            if (!/\.jsonc?$/.test(file_name))
+                return;
             try {
                 const file_path = path.join("./src/filter/", filter.dir, file_name);
                 const content = fs.readFileSync(file_path, "utf8");
                 const content_json = parse(content.toString());
-                const lint_result = lint(content_json);
+                const lint_result = lint.run(content_json);
                 if (lint_result[0])
                     filter_content.push(content_json);
                 else {
@@ -95,7 +107,7 @@ try {
                 }
             }
             catch (e) {
-                console.log(e);
+                console.error(e);
             }
         });
         let joined_filter;
@@ -107,21 +119,25 @@ try {
         try {
             fs.writeFileSync(path.join("./dist/filter", `${filter.dir}.json`), JSON.stringify(joined_filter, null, 4), "utf8");
             advanced_filter[filter.name] = {
+<<<<<<< HEAD
                 url: `https://cdn.statically.io/gh/Robot-Inventor/stc-filter/main/dist/filter/${filter.dir}.json?dev=1`,
+=======
+                url: generate_cdn_url(`dist/filter/${filter.dir}.json`),
+>>>>>>> 828fa79 (フィルターの形式のリントの実装を改善 #113)
                 id: filter.id
             };
         }
         catch (e) {
-            console.log(e);
+            console.error(e);
         }
     });
     try {
         fs.writeFileSync("./dist/advanced_filter.json", JSON.stringify(advanced_filter, null, 4), "utf8");
     }
     catch (e) {
-        console.log(e);
+        console.error(e);
     }
 }
 catch (e) {
-    console.log(e);
+    console.error(e);
 }
